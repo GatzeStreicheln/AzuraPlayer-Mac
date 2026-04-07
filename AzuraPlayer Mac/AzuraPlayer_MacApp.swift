9673eb226da922c478cd8116d61ff4f21ff7f671
GatzeStreicheln <login@gatze.ch>
2026-04-07 22:56:29 +0200

    Initial Commit

---
+++ AzuraPlayer Mac/AzuraPlayer_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzuraPlayer_MacApp.swift
+//  AzuraPlayer Mac
+//
+//  Created by Vasco Kugler on 07.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AzuraPlayer_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
